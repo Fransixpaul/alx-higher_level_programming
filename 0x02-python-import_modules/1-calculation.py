255f7be81b4d944d1d70fc2a030e9623062e8288
francis <kayininse@gmail.com>
2024-03-31 02:21:50 +0100

    My first toolbox!

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,7 +2,7 @@
 a = 10
 b = 5
 if __name__ == "__main__":
-    from calculator_1 import add, sub, mul ,div
+    from calculator_1 import add, sub, mul, div
     result_add = add(a, b)
     result_sub = sub(a, b)
     result_mul = mul(a, b)
