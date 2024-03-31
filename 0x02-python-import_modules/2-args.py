76cf529baa404e75ceac087b95b6914d03189d4c
francis <kayininse@gmail.com>
2024-03-31 15:13:21 +0100

    Prints the number of the list of Arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,7 +6,6 @@ if __name__ == "__main__":
 
     if num_args == 0:
         print("0 arguments.")
-        print(".")
     else:
         print(f"{num_args} argument{'s' if num_args > 1 else ''}:")
         for idx, arg in enumerate(argv[1:], start=1):
