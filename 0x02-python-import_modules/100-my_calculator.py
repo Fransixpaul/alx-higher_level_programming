017ea914d3ec34ffd5c600a9c00e59dc1c180459
francis <kayininse@gmail.com>
2024-04-02 00:41:10 +0100

    Build my own calculator!

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,10 +2,12 @@
 import sys
 from calculator_1 import add, sub, mul, div
 
+
 def main():
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
+
 
     a = int(sys.argv[1])
     operator = sys.argv[2]
@@ -23,7 +25,7 @@ def main():
             sys.exit(1)
         result = div(a, b)
     else:
-        print("Unknown operator. Available operators: +, -, *, and /")
+        print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
     print("{} {} {} = {}".format(a, operator, b, result))
