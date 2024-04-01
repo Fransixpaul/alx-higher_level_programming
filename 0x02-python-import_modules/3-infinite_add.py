6b422551e33b73280e7730cda4eacb7da08bf33d
francis <kayininse@gmail.com>
2024-04-01 23:10:25 +0100

    Infinite addition

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import sys
+def add_arguments(args):
+    total = sum(int(arg) for arg in args)
+    print(total)
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    add_arguments(args)
