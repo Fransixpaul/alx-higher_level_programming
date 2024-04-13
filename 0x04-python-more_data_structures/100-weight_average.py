bbfa09a11b3d461774ba5f930ad111c45324e01a
francis <kayininse@gmail.com>
2024-04-13 17:28:10 +0100

    Calculates the average value of all integer tuple

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -6,11 +6,9 @@ def weight_average(my_list=[]):
     score_sum = 0
     weight_sum = 0
 
-    
     for score, weight in my_list:
         score_sum += score * weight
         weight_sum += weight
-
 
     weighted_average = score_sum / weight_sum
     return weighted_average
