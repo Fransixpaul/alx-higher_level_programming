79368b5ad91b2725a209961b44534a418a07099f
francis <kayininse@gmail.com>
2024-04-27 15:53:03 +0100

    All commit

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 """This module defines the Square class"""
 
-class Square:
-    """
-    Defines a square. 
 
-    Attributes:
-    - No attributes defined at the moment.
-    """
+class Square:
+    """Defines a square"""
