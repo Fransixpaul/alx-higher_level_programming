6ceeee304f8b032b8f4a6d3d0dde45e56f0c132e
francis <kayininse@gmail.com>
2024-07-09 00:55:12 +0100

    Error code #1

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -11,7 +11,7 @@ import requests
 def main():
     url = sys.argv[1]
 
-    response = requests.get.(url)
+    response = requests.get(url)
 
     if response.status_code >= 400:
         print(f"Error code: {response.status_code}")
