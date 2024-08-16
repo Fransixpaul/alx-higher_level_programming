e36cae867be5e12ca81de49be908ad4cd45871b6
francis <krypton@gmail.com>
2024-08-16 14:49:12 +0100

    Factor index

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
-/* imports an array and computes a new array  */
-const array = require('./100-data').list;
-console.log(array);
-console.log(array.map((item, list) => item * list));
+
+const { list } = require('./100-data');
+
+console.log(list);
+console.log(list.map((element, idx) => element * idx));
