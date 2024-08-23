5b2bf6f506338955e3fabb5957e050c53df6563e
francis <krypton@gmail.com>
2024-08-23 14:58:38 +0100

    Integers addition

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-""" a function that computes the addition of two ints"""
+"""Integers addition"""
 
 
 def add_integer(a, b=98):
-    """it returns the integer addition of a and b """
-    if not isinstance(a, int) and not isinstance(a, float):
+    """Adds two numbers
+    Args:
+        a - first number input
+        b - second number input
+    """
+    if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
-    if not isinstance(b, int) and not isinstance(b, float):
+    if type(b) not in [int, float]:
         raise TypeError("b must be an integer")
-    if type(a) == float:
-        a = int(a)
-    if type(b) == float:
-        b = int(b)
-    return (int(a) + int(b))
+    return int(a) + int(b)
