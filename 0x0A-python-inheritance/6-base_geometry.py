d443c45fe2624b1cb396dbc7bbbb51cfa8bcfaba
francis <krypton@gmail.com>
2024-08-30 17:31:03 +0100

    python inheritance

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+n new class based on 5-base_geometry.py """
+
+
+class BaseGeometry:
+    """ a defined class which is ment to be a base class """
+    def area(self):
+        """ a public instance method that raises an exception """
+        raise Exception("area() is not implemented")
